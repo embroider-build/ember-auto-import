8ab22cad50a2b6a70dcdbdf9183ff5642f59292d
Edward Faulkner <ef@alum.mit.edu>
2018-06-10 03:22:09 -0400

    progress on switching to preprocess-registry-based analyzer

--- index.js
+++ index.js
@@ -5,67 +5,68 @@ const DepFinder = require('./lib/dep-finder');
 const Bundler = require('./lib/bundler');
 const MergeTrees = require('broccoli-merge-trees');
 const debugTree = require('broccoli-debug').buildDebugCallback('ember-auto-import');
-const concat = require('broccoli-concat');
 
 module.exports = {
   name: 'ember-auto-import',
 
-  included() {
-    this._depFinder = new DepFinder(this.parent);
-    this.import(`${this.parent.pkg.name}/ember-auto-imports.js`);
-    if (this.project === this.parent) {
-      this.import(`${this.parent.pkg.name}/ember-auto-imports-test.js`, { type: 'test' });
+  setupPreprocessorRegistry(type, registry) {
+    // we register on our parent registry (so we will process code
+    // from the app or addon that chose to include us) rather than our
+    // own registry (which would cause us to process our own code)
+    if (type !== 'parent') {
+      return;
     }
+
+    registry.add('js', {
+      name: 'ember-auto-import-analyzer',
+      toTree: (tree) => {
+        // The analyzer is responsible for identifying the set of
+        // things that are being imported by app code. We need to do
+        // shenanigans to link the analyzer and the bundler together.
+
+        if (!this._connectAnalyzer) {
+          throw new Error('bug in ember-auto-import: expected bundler to be instantiated before analyzer');
+        }
+        return new Analyzer(debugTree(tree, `preprocessor:input:${counter++}`), {
+          connectAnalyzer: this._connectAnalyzer
+        });
+      }
+    });
   },
 
-  postprocessTree: function(type, tree){
-    let outputFile;
-    if (type === 'js'){
-      outputFile = `${this.parent.pkg.name}/ember-auto-imports.js`;
-    } else if (type === 'test') {
-      outputFile = `${this.parent.pkg.name}/ember-auto-imports-test.js`;
-    }
+  included() {
+    // When consumed by an addon, we will have
+    // this.parent.options. When consumed by an app, we will have
+    // this.app.options.
+    this._usedByAddon = !!this.parent.options;
+    this._options = this.parent.options || this.app.options;
+    this._depFinder = new DepFinder(this.parent);
 
-    if (outputFile) {
+    // This namespacing ensures we can be used by multiple packages as
+    // well as by an addon and its dummy app simultaneously
+    this._namespace = `${this.parent.pkg.name}/${this._usedByAddon ? 'addon' : 'app'}`;
 
-      // The analyzer is responsible for identifying the set of things
-      // that are being imported by app code.
-      let analyzer = new Analyzer(tree);
+    this.import(`vendor/${this._namespace}/ember-auto-imports.js`);
+    //this.import(`vendor/${this._namespace}/ember-auto-imports-test.js`, { type: 'test' });
+  },
 
-      // The bundler is responsible for determining which imported
-      // modules discovered by the analyzer are external NPM packages
-      // that need to be handled by auto-import, and packaging them
-      // into AMD-loader compatible format.
-      //
-      // The bundler gets the analyzer's output as input (in order to
-      // express their dependency relationship to broccoli). And we
-      // also give the bundler a direct reference to the analyzer,
-      // because they communicate over their own direct protocol.
-      let bundler = new Bundler(analyzer, {
-        analyzer,
-        depFinder: this._depFinder,
-        config: this.app.options.autoImport
-      });
+  treeForVendor(tree) {
+    // The bundler is responsible for determining which imported
+    // modules discovered by the analyzer are external NPM packages
+    // that need to be handled by auto-import, and packaging them
+    // into AMD-loader compatible format.
+    let bundler = new Bundler({
+      outputFile: `${this._namespace}/ember-auto-imports.js`,
+      depFinder: this._depFinder,
+      config: this._options.autoImport
+    });
 
-      bundler = debugTree(bundler, 'bundler');
-
-      // The bundler generates one file per imported module, here we
-      // combine them into a single file so we can share a single
-      // constant app.import.
-      let combined = concat(bundler, {
-        outputFile,
-        inputFiles: ['**/*'],
-        sourceMapConfig: { enabled: true },
-        allowNone: true
-      });
-
-      combined = debugTree(combined, 'combined');
-
-      tree = new MergeTrees([
-        tree,
-        combined
-      ]);
-    }
-    return tree;
+    this._connectAnalyzer = bundler.connectAnalyzer.bind(bundler);
+    return new MergeTrees([
+      tree,
+      debugTree(bundler.tree, 'combined')
+    ]);
   }
 };
+
+let counter = 0;
