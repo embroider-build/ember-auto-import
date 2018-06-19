ebc3e67427094d162004156b45a78d0f1e30dec3
Edward Faulkner <ef@alum.mit.edu>
2018-06-19 14:19:36 -0400

    keep our private state off the public addon instance

--- ts/index.ts
+++ ts/index.ts
@@ -11,6 +11,21 @@ import webpackBundler from './webpack';
 const debugTree = buildDebugCallback('ember-auto-import');
 const testsPattern = new RegExp(`^(/tests)?/[^/]+/(tests|test-support)/`);
 
+class PrivateState {
+  env: string;
+  depFinder: DepFinder;
+  analyzer: Analyzer;
+  babelOptions;
+  namespace: string;
+  config;
+
+  constructor(publicInstance){
+    priv.set(publicInstance, this);
+  }
+}
+
+const priv = new WeakMap<any, PrivateState>();
+
 module.exports = {
   name: 'ember-auto-import',
 
@@ -30,58 +45,61 @@ module.exports = {
     registry.add('js', {
       name: 'ember-auto-import-analyzer',
       toTree: (tree, inputPath) => {
-        return this._analyzer.analyzeTree(debugTree(tree, `preprocessor:input`), inputPath);
+        let ps = priv.get(this);
+        return ps.analyzer.analyzeTree(debugTree(tree, `preprocessor:input`), inputPath);
       }
     });
   },
 
   included() {
     this._super.included.apply(this, arguments);
+    let ps = new PrivateState(this);
 
     // When consumed by an addon, we will have
     // this.parent.options. When consumed by an app, we will have
     // this.app.options.
-    this._usedByAddon = !!this.parent.options;
+    let usedByAddon = !!this.parent.options;
     let options = this.parent.options || this.app.options;
 
     // _findHost is private API but it's been stable in ember-cli for two years.
-    this._env = this._findHost().env;
+    ps.env = this._findHost().env;
 
-    if (!this._env) { throw new Error("Bug in ember-auto-import: did not discover environment"); }
-    this._depFinder = new DepFinder(this.parent, this._usedByAddon);
+    if (!ps.env) { throw new Error("Bug in ember-auto-import: did not discover environment"); }
+    ps.depFinder = new DepFinder(this.parent, usedByAddon);
 
     // Generate the same babel options that the consuming app or addon
     // is using. We will use these so we can configure our parser to
     // match.
     let babelAddon = this.addons.find(addon => addon.name === 'ember-cli-babel');
-    this._babelOptions = babelAddon.buildBabelOptions(options);
+    ps.babelOptions = babelAddon.buildBabelOptions(options);
 
     // Stash our own config options
-    this._config = options.autoImport || {};
-    if (!this._config.modules) {
-      this._config.modules = Object.create(null);
+    ps.config = options.autoImport || {};
+    if (!ps.config.modules) {
+      ps.config.modules = Object.create(null);
     }
 
     // https://github.com/babel/ember-cli-babel/issues/227
-    delete this._babelOptions.annotation;
-    delete this._babelOptions.throwUnlessParallelizable;
-    if (this._babelOptions.plugins) {
-      this._babelOptions.plugins = this._babelOptions.plugins.filter(p => !p._parallelBabel);
+    delete ps.babelOptions.annotation;
+    delete ps.babelOptions.throwUnlessParallelizable;
+    if (ps.babelOptions.plugins) {
+      ps.babelOptions.plugins = ps.babelOptions.plugins.filter(p => !p._parallelBabel);
     }
 
     // This namespacing ensures we can be used by multiple packages as
     // well as by an addon and its dummy app simultaneously
-    this._namespace = `${this.parent.pkg.name}/${this._usedByAddon ? 'addon' : 'app'}`;
+    ps.namespace = `${this.parent.pkg.name}/${usedByAddon ? 'addon' : 'app'}`;
 
-    this.import(`vendor/${this._namespace}/ember-auto-imports.js`);
-    this.import(`vendor/${this._namespace}/ember-auto-imports-test.js`, { type: 'test' });
+    this.import(`vendor/${ps.namespace}/ember-auto-imports.js`);
+    this.import(`vendor/${ps.namespace}/ember-auto-imports-test.js`, { type: 'test' });
   },
 
   treeForVendor(tree) {
+    let ps = priv.get(this);
 
     // The Analyzer keeps track of all your imports
-    this._analyzer = Analyzer.create(
-      this._babelOptions,
+    ps.analyzer = Analyzer.create(
+      ps.babelOptions,
       (tree) => {
         // Here be dragons
         appBundler.unsafeConnect(tree);
@@ -92,9 +110,9 @@ module.exports = {
     // The Splitter takes the set of imports from the Analyzer and
     // decides which ones to include in which bundles
     let splitter = new Splitter({
-      depFinder: this._depFinder,
-      config: this._config,
-      analyzer: this._analyzer,
+      depFinder: ps.depFinder,
+      config: ps.config,
+      analyzer: ps.analyzer,
       bundles: ['app', 'tests'],
       bundleForPath(path) {
         if (testsPattern.test(path)) {
@@ -109,20 +127,20 @@ module.exports = {
     // are responsible for packaging those deps up.
 
     let appBundler = new Bundler({
-      outputFile: `${this._namespace}/ember-auto-imports.js`,
+      outputFile: `${ps.namespace}/ember-auto-imports.js`,
       splitter,
       bundle: 'app',
-      config: this._config,
-      environment: this._env,
+      config: ps.config,
+      environment: ps.env,
       consoleWrite: (...args) => this.project.ui.write(...args)
     });
 
     let testsBundler = new Bundler({
-      outputFile: `${this._namespace}/ember-auto-imports-test.js`,
+      outputFile: `${ps.namespace}/ember-auto-imports-test.js`,
       splitter,
       bundle: 'tests',
-      config: this._config,
-      environment: this._env,
+      config: ps.config,
+      environment: ps.env,
       consoleWrite: (...args) => this.project.ui.write(...args)
     });
 
