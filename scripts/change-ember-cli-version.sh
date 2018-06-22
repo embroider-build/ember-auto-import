ebede3741fa4ff442547b11ab45cf838a6770edb
Edward Faulkner <ef@alum.mit.edu>
2018-06-22 01:06:07 -0400

    adding multi-version coverage in travis

---
+++ scripts/change-ember-cli-version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+case "$EAI_SCENARIO" in 
+    "2.18")
+        yarn add --dev ember-cli@^2.18.0
+        ;;
+    "3.0")
+        yarn add --dev ember-cli@^3.0.0
+        ;;
+    "stable")
+        yarn add --dev ember-cli@latest
+        ;;
+    "beta")
+        yarn add --dev ember-cli@beta
+        ;;
+    "canary")
+        yarn add --dev ember-cli@git+https://github.com/ember-cli/ember-cli#master
+        ;;
+    *)
+        yarn
+        ;;
+esac
+
