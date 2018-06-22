6f4d101f49034ed72e77be975fd82c183f0a9c2a
Edward Faulkner <ef@alum.mit.edu>
2018-06-22 01:19:56 -0400

    more versions, and no more curl install of gnu parallel

--- scripts/change-ember-cli-version.sh
+++ scripts/change-ember-cli-version.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
 case "$EAI_SCENARIO" in 
+    "2.12")
+        yarn add --dev ember-cli@^2.12.0
+        ;;
+    "2.16")
+        yarn add --dev ember-cli@^2.16.0
+        ;;
     "2.18")
         yarn add --dev ember-cli@^2.18.0
         ;;
+        
     "3.0")
         yarn add --dev ember-cli@^3.0.0
         ;;
