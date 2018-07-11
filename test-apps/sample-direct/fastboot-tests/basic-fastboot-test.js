1c2c16fd9c04fe0b67a5d694bced3ab24fdbafab
Edward Faulkner <ef@alum.mit.edu>
2018-07-11 03:36:55 -0400

    add failing test for lazy-loaded deps in fastboot

--- test-apps/sample-direct/fastboot-tests/basic-fastboot-test.js
+++ test-apps/sample-direct/fastboot-tests/basic-fastboot-test.js
@@ -1,6 +1,6 @@
 const FastBoot = require('fastboot');
 const { execFileSync } = require('child_process');
-const { module: Qmodule, test } = require('qunit');
+const { module: Qmodule, test, skip } = require('qunit');
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
@@ -28,6 +28,13 @@ Qmodule('sample-direct | fastboot', function(hooks) {
     let html = await page.html();
     let document = new JSDOM(html).window.document;
     assert.equal(document.querySelector('.hello-world').textContent.trim(), '2018-05-31', 'expected moment to work');
+  })
+
+  skip('lazy loaded deps', async function(assert) {
+    let page = await fastboot.visit('/dynamic-import');
+    let html = await page.html();
+    let document = new JSDOM(html).window.document;
+    assert.equal(document.querySelector('[data-test="dynamic-import-result"]').textContent.trim(), 'ember-auto-import-a-dependency');
   })
 
 });
