4818ea599afbcaa16bf99c09dbf023674100730d
Edward Faulkner <ef@alum.mit.edu>
2018-08-15 15:31:31 -0400

    test coverage for prod build

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,6 +3,7 @@
 ./scripts/parallel --tag <<EOF
 cd test-apps/sample-direct   && yarn test
 cd test-apps/sample-direct   && yarn test:fastboot
+cd test-apps/sample-direct   && yarn test:prod
 cd test-apps/sample-indirect && yarn test
 cd test-apps/sample-indirect && yarn test:fastboot
 cd test-apps/sample-addon    && yarn test
