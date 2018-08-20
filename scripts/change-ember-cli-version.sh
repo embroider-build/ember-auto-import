acf8c270a404f46f5f0e4ecfd9533885f6800821
Edward Faulkner <ef@alum.mit.edu>
2018-08-20 10:45:33 -0400

    fix test coverage
    It wasn't really testing 2.12 because the caret is too permissive.

--- scripts/change-ember-cli-version.sh
+++ scripts/change-ember-cli-version.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
-case "$EAI_SCENARIO" in 
+case "$EAI_SCENARIO" in
     "2.12")
-        yarn add --dev ember-cli@^2.12.0
+        yarn add --dev ember-cli@~2.12.0
         ;;
     "2.18")
-        yarn add --dev ember-cli@^2.18.0
+        yarn add --dev ember-cli@~2.18.0
         ;;
     "stable")
         yarn add --dev ember-cli@latest
