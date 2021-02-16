0e801d12fe0f77524cb8e74e050bbfffba938524
Edward Faulkner <ef@alum.mit.edu>
2021-02-16 00:21:13 -0500

    adjusting commands

--- packages/ember-auto-import/config/ember-try.js
+++ packages/ember-auto-import/config/ember-try.js
@@ -1,67 +1,45 @@
-'use strict';
-
 const getChannelURL = require('ember-source-channel-url');
 
-module.exports = function () {
-  return Promise.all([getChannelURL('release'), getChannelURL('beta'), getChannelURL('canary')]).then(urls => {
-    return {
-      useYarn: true,
-      scenarios: [
-        {
-          name: 'ember-lts-2.12',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.12.0',
-            },
+module.exports = async function () {
+  return {
+    useYarn: true,
+    scenarios: [
+      {
+        name: 'ember-lts-2.18',
+        npm: {
+          devDependencies: {
+            'ember-source': '~2.18.0',
+            'ember-cli': '~2.18.0',
           },
         },
-        {
-          name: 'ember-lts-2.16',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.16.0',
-            },
+      },
+      {
+        name: 'ember-release',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('release'),
+            'ember-cli': 'latest',
           },
         },
-        {
-          name: 'ember-lts-2.18',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.18.0',
-            },
+      },
+      {
+        name: 'ember-beta',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('beta'),
+            'ember-cli': 'beta',
           },
         },
-        {
-          name: 'ember-release',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[0],
-            },
+      },
+      {
+        name: 'ember-canary',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('canary'),
+            'ember-cli': 'git+https://github.com/ember-cli/ember-cli#master',
           },
         },
-        {
-          name: 'ember-beta',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[1],
-            },
-          },
-        },
-        {
-          name: 'ember-canary',
-          npm: {
-            devDependencies: {
-              'ember-source': urls[2],
-            },
-          },
-        },
-        {
-          name: 'ember-default',
-          npm: {
-            devDependencies: {},
-          },
-        },
-      ],
-    };
-  });
+      },
+    ],
+  };
 };
