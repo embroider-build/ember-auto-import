4e138fcf15e9565a1e084b3010b09bf3a8033a4b
Edward Faulkner <ef@alum.mit.edu>
2021-03-05 12:30:16 -0500

    moving app-template under packages

--- packages/app-template/config/environment.js
+++ packages/app-template/config/environment.js
@@ -1,8 +1,8 @@
 'use strict';
 
-module.exports = function(environment) {
+module.exports = function (environment) {
   let ENV = {
-    modulePrefix: 'app-template',
+    modulePrefix: '@ef4/app-template',
     environment,
     rootURL: '/',
     locationType: 'auto',
@@ -13,14 +13,14 @@ module.exports = function(environment) {
       },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
+        Date: false,
+      },
     },
 
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
   };
 
   if (environment === 'development') {
--- test-scenarios/scenarios.ts
+++ test-scenarios/scenarios.ts
@@ -1,14 +1,16 @@
 import { join } from 'path';
 import { Scenarios, Project } from '@ef4/test-support';
+import { dirname } from 'path';
 
 async function beta(project: Project) {
   project.linkDependency('ember-cli', { baseDir: __dirname, resolveName: 'ember-cli-beta' });
 }
 
-export const appScenarios = Scenarios.fromDir(join(__dirname, '..', 'app-template'))
+export const appScenarios = Scenarios.fromDir(dirname(require.resolve('@ef4/app-template/package.json')))
   .add('default', () => {})
   .add('beta', beta);
 
+// TODO this path doesn't exist yet, create a real package analogous to @ef4/app-template
 export const addonScenarios = Scenarios.fromDir(join(__dirname, '..', 'addon-template'))
   .add('default', () => {})
   .add('beta', beta);
