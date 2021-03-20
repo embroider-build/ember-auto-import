4f4d328ce9eaf670b3e0cc9a3212fac0e9df2f31
Edward Faulkner <ef@alum.mit.edu>
2021-03-20 14:28:46 -0400

    running own tests and node tests via the new test infra

--- test-scenarios/scenarios.ts
+++ test-scenarios/scenarios.ts
@@ -5,10 +5,23 @@ async function beta(project: Project) {
   project.linkDependency('ember-cli', { baseDir: __dirname, resolveName: 'ember-cli-beta' });
 }
 
-export const appScenarios = Scenarios.fromDir(dirname(require.resolve('@ef4/app-template/package.json')))
-  .add('default', () => {})
-  .add('beta', beta);
+const supported = {
+  default: () => {},
+  beta,
+};
 
-export const addonScenarios = Scenarios.fromDir(dirname(require.resolve('@ef4/addon-template/package.json')))
-  .add('default', () => {})
-  .add('beta', beta);
+export function supportMatrix(scenarios: Scenarios) {
+  let s = scenarios;
+  for (let [name, mutator] of Object.entries(supported)) {
+    s = s.add(name, mutator);
+  }
+  return s;
+}
+
+export const appScenarios = supportMatrix(
+  Scenarios.fromDir(dirname(require.resolve('@ef4/app-template/package.json')))
+);
+
+export const addonScenarios = supportMatrix(
+  Scenarios.fromDir(dirname(require.resolve('@ef4/addon-template/package.json')))
+);
