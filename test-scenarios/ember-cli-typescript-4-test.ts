b3de256cfb66255bd2aa746d0f0946be86261914
Edward Faulkner <ef@alum.mit.edu>
2021-03-23 22:09:35 -0400

    re-add where actually wanted

--- test-scenarios/ember-cli-typescript-4-test.ts
+++ test-scenarios/ember-cli-typescript-4-test.ts
@@ -17,6 +17,7 @@ appScenarios
       },
     });
     project.addDevDependency(aDependency);
+    project.linkDevDependency('ember-auto-import', { baseDir: __dirname });
     project.linkDevDependency('ember-cli-typescript', { baseDir: __dirname, resolveName: 'ember-cli-typescript-4' });
     project.linkDevDependency('typescript', { baseDir: __dirname, resolveName: 'typescript-4' });
 
