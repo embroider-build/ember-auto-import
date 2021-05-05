6fa2ab77ff7b9355266387104aec86242b2551e6
Edward Faulkner <ef@alum.mit.edu>
2021-05-05 18:08:35 -0400

    Run inserter before asset-rev
    And refactor the inserter to provide a safety check for this class of problem.

--- packages/ember-auto-import/ts/inserter.ts
+++ packages/ember-auto-import/ts/inserter.ts
@@ -18,10 +18,11 @@ export class Inserter extends Plugin {
   }
   async build() {
     let fastbootInfo = this.fastbootManifestInfo();
+    let chunks = categorizeChunks(this.bundler.buildResult, this.config);
     for (let filename of this.config.htmlEntrypoints()) {
       let fullName = join(this.inputPaths[0], filename);
       if (existsSync(fullName)) {
-        this.processHTML(filename, fullName, fastbootInfo);
+        this.processHTML(filename, fullName, fastbootInfo, chunks);
       }
     }
 
@@ -44,41 +45,23 @@ export class Inserter extends Plugin {
   private processHTML(
     filename: string,
     fullName: string,
-    fastbootInfo: ReturnType<typeof Inserter.prototype.fastbootManifestInfo>
+    fastbootInfo: ReturnType<typeof Inserter.prototype.fastbootManifestInfo>,
+    chunks: Chunks
   ) {
     debug(`parsing %s`, filename);
     let html = readFileSync(fullName, 'utf8');
     let ast = parse5.parse(html, { sourceCodeLocationInfo: true });
-    let { scripts, styles } = chunks(this.bundler.buildResult, this.config);
     let stringInserter = new StringInserter(html);
 
-    debug(`looking for scripts: %s`, [...scripts.keys()]);
-    debug(`looking for styles: %s`, [...styles.keys()]);
+    debug(`looking for scripts: %s`, [...chunks.scripts.keys()]);
+    debug(`looking for styles: %s`, [...chunks.styles.keys()]);
 
     traverse(ast, element => {
       if (element.tagName === 'script') {
         let src = element.attrs.find(a => a.name === 'src')?.value;
         if (src) {
           debug(`found script with src=%s`, src);
-          for (let [url, { chunks, bundleName }] of scripts) {
-            if (src.endsWith(url)) {
-              debug(`inserting %s`, chunks);
-              let rootURL = src.replace(url, '');
-              let insertedSrc = chunks.map(chunk => `\n<script src="${rootURL}${chunk}"></script>`).join('');
-              if (fastbootInfo?.readsHTML && bundleName === 'app') {
-                // lazy chunks are eager in fastboot because webpack's lazy
-                // loading doesn't work in fastboot, because we share a single
-                // build with the browser and use a browser-specific
-                // lazy-loading implementation. It's probably better to make
-                // them eager on the server anyway, so they're handled as part
-                // of server startup.
-                insertedSrc += this.bundler.buildResult.lazyAssets
-                  .map(chunk => `\n<fastboot-script src="${rootURL}${chunk}"></fastboot-script>`)
-                  .join('');
-              }
-              stringInserter.insert(element.sourceCodeLocation!.endOffset, insertedSrc);
-            }
-          }
+          this.insertScripts(chunks, fastbootInfo, stringInserter, element, src);
         }
       }
 
@@ -87,21 +70,68 @@ export class Inserter extends Plugin {
           let href = element.attrs.find(a => a.name === 'href')?.value;
           if (href) {
             debug(`found stylesheet with href=%s`, href);
-            for (let [url, { chunks }] of styles) {
-              if (href.endsWith(url)) {
-                debug(`inserting %s`, chunks);
-                let rootURL = href.replace(url, '');
-                stringInserter.insert(
-                  element.sourceCodeLocation!.endOffset,
-                  chunks.map(chunk => `\n<link rel="stylesheet" href="${rootURL}${chunk}"/>`).join('')
-                );
-              }
-            }
+            this.insertStyles(chunks, stringInserter, element, href);
           }
         }
       }
     });
+
+    let appScripts = [...chunks.scripts.values()].find(entry => entry.bundleName === 'app');
+    if (appScripts && !appScripts.inserted) {
+      throw new Error(`ember-auto-import could not find a place to insert app scripts in ${filename}.`);
+    }
+
+    let appStyles = [...chunks.styles.values()].find(entry => entry.bundleName === 'app');
+    if (appStyles && !appStyles.inserted) {
+      throw new Error(`ember-auto-import could not find a place to insert app styles in ${filename}.`);
+    }
+
     outputFileSync(join(this.outputPath, filename), stringInserter.serialize(), 'utf8');
+  }
+
+  private insertScripts(
+    chunks: Chunks,
+    fastbootInfo: ReturnType<typeof Inserter.prototype.fastbootManifestInfo>,
+    stringInserter: StringInserter,
+    element: parse5.Element,
+    src: string
+  ) {
+    for (let [url, entry] of chunks.scripts) {
+      if (src.endsWith(url)) {
+        let { scriptChunks, bundleName } = entry;
+        entry.inserted = true;
+        debug(`inserting %s`, scriptChunks);
+        let rootURL = src.replace(url, '');
+        let insertedSrc = scriptChunks.map(chunk => `\n<script src="${rootURL}${chunk}"></script>`).join('');
+        if (fastbootInfo?.readsHTML && bundleName === 'app') {
+          // lazy chunks are eager in fastboot because webpack's lazy
+          // loading doesn't work in fastboot, because we share a single
+          // build with the browser and use a browser-specific
+          // lazy-loading implementation. It's probably better to make
+          // them eager on the server anyway, so they're handled as part
+          // of server startup.
+          insertedSrc += this.bundler.buildResult.lazyAssets
+            .map(chunk => `\n<fastboot-script src="${rootURL}${chunk}"></fastboot-script>`)
+            .join('');
+        }
+        stringInserter.insert(element.sourceCodeLocation!.endOffset, insertedSrc);
+      }
+    }
+  }
+
+  private insertStyles(chunks: Chunks, stringInserter: StringInserter, element: parse5.Element, href: string) {
+    for (let [url, entry] of chunks.styles) {
+      if (href.endsWith(url)) {
+        let { styleChunks } = entry;
+        entry.inserted = true;
+        debug(`inserting %s`, styleChunks);
+        let rootURL = href.replace(url, '');
+        stringInserter.insert(
+          element.sourceCodeLocation!.endOffset,
+          styleChunks.map(chunk => `\n<link rel="stylesheet" href="${rootURL}${chunk}"/>`).join('')
+        );
+      }
+    }
   }
 
   private fastbootManifestInfo():
@@ -130,18 +160,23 @@ export class Inserter extends Plugin {
   }
 }
 
-function chunks(buildResult: BuildResult, config: BundleConfig) {
-  let scripts: Map<string, { chunks: string[]; bundleName: BundleName }> = new Map();
-  let styles: Map<string, { chunks: string[]; bundleName: BundleName }> = new Map();
+interface Chunks {
+  scripts: Map<string, { scriptChunks: string[]; bundleName: BundleName; inserted: boolean }>;
+  styles: Map<string, { styleChunks: string[]; bundleName: BundleName; inserted: boolean }>;
+}
+
+function categorizeChunks(buildResult: BuildResult, config: BundleConfig): Chunks {
+  let scripts: Chunks['scripts'] = new Map();
+  let styles: Chunks['styles'] = new Map();
 
   for (let [bundleName, assets] of buildResult.entrypoints) {
     let scriptChunks = assets.filter(a => a.endsWith('.js'));
     if (scriptChunks.length > 0) {
-      scripts.set(config.bundleEntrypoint(bundleName, 'js'), { chunks: scriptChunks, bundleName });
+      scripts.set(config.bundleEntrypoint(bundleName, 'js'), { scriptChunks, bundleName, inserted: false });
     }
     let styleChunks = assets.filter(a => a.endsWith('.css'));
     if (styleChunks.length > 0) {
-      styles.set(config.bundleEntrypoint(bundleName, 'css'), { chunks: styleChunks, bundleName });
+      styles.set(config.bundleEntrypoint(bundleName, 'css'), { styleChunks, bundleName, inserted: false });
     }
   }
   return { scripts, styles };
