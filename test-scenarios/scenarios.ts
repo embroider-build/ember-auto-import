7d7316b54b50a1422484b40a334ec3f3b707ef85
Edward Faulkner <ef@alum.mit.edu>
2021-08-18 13:42:14 -0400

    update tests to match current version and add a safety check

--- test-scenarios/scenarios.ts
+++ test-scenarios/scenarios.ts
@@ -16,6 +16,15 @@ import { merge } from 'lodash';
   process.env['PATH'] = paths.join(delimiter);
 })();
 
+if (
+  require('../packages/ember-auto-import/package.json').version !==
+  require('./package.json').devDependencies['ember-auto-import']
+) {
+  throw new Error(
+    `version safety check failure. test-scenarios is not depending on the current verion of ember-auto-import`
+  );
+}
+
 async function lts(project: Project) {
   project.linkDevDependency('ember-cli', { baseDir: __dirname, resolveName: 'ember-cli-lts' });
   project.linkDevDependency('ember-source', { baseDir: __dirname, resolveName: 'ember-source-lts' });
