391f165c54547f646b6975c9ee0c09c0ef7a5643
Simon Ihmig <simon.ihmig@gmail.com>
2024-05-01 15:15:20 +0200

    Add getImportableModules util

--- packages/ember-auto-import/ts/util.ts
+++ packages/ember-auto-import/ts/util.ts
@@ -1,5 +1,7 @@
 import isSubdir from 'is-subdir';
 import { dirname } from 'path';
+// @ts-expect-error types don't resolve
+import { getPackageEntryPoints } from 'pkg-entry-points';
 
 export function shallowEqual(a: any[], b: any[]) {
   return (
@@ -32,4 +34,23 @@ export function commonAncestorDirectories(dirs: string[]): string[] {
 
     return newResults;
   }, [] as string[]);
+}
+
+export async function getImportableModules(
+  packagePath: string
+): Promise<string[]> {
+  const entryPoints = await getPackageEntryPoints(packagePath);
+
+  return Object.values(entryPoints)
+    .map(
+      (alternatives) =>
+        alternatives.find(
+          ([conditions]) =>
+            (conditions.includes('import') || conditions.includes('default')) &&
+            !conditions.includes('types') &&
+            !conditions.includes('node')
+        )?.[1]
+    )
+    .filter((item): item is string => !!item)
+    .filter((item, index, array) => array.indexOf(item) === index);
 }
