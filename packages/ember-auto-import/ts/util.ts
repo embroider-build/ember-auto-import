a0a8021185dd879e5ef23822e29e26d3f82095e1
Simon Ihmig <simon.ihmig@gmail.com>
2024-05-01 12:24:57 +0200

    Add commonAncestorDirectories util

--- packages/ember-auto-import/ts/util.ts
+++ packages/ember-auto-import/ts/util.ts
@@ -1,3 +1,6 @@
+import isSubdir from 'is-subdir';
+import { dirname } from 'path';
+
 export function shallowEqual(a: any[], b: any[]) {
   return (
     a &&
@@ -9,4 +12,24 @@ export function shallowEqual(a: any[], b: any[]) {
 
 export function stripQuery(path: string) {
   return path.split('?')[0];
+}
+
+export function commonAncestorDirectories(dirs: string[]): string[] {
+  return dirs.reduce((results, fileOrDir) => {
+    let dir = dirname(fileOrDir);
+
+    if (results.length === 0) {
+      return [dir];
+    }
+
+    let newResults = results.filter(
+      (existingDir) => !isSubdir(dir, existingDir)
+    );
+
+    if (!newResults.some((existingDir) => isSubdir(existingDir, dir))) {
+      newResults.push(dir);
+    }
+
+    return newResults;
+  }, [] as string[]);
 }
