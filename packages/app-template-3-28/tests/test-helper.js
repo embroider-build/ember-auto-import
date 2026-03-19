cfb2cc0aa731e1ebbea2af000bc1feac873a84a7
Katie Gengler <katie@kmg.io>
2026-03-19 16:12:02 -0400

    Add app-template for 3.28

---
+++ packages/app-template-3-28/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'app-template-3-28/app';
+import config from 'app-template-3-28/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
